8c035229c1ee0e920fea58e6da5404325c82cab2
numa <aytdm251@gmail.com>
2018-08-31 18:12:39 +0900

    Change error message

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="error-container">
-    <h1 v-if="error.statusCode === 404">Page not found ...(☍﹏⁰) </h1>
-    <h1 v-else>An error occured </h1>
+    <h1>An error occured ...(☍﹏⁰) </h1>
     <nuxt-link to="/">Back Top</nuxt-link>
   </div>
 </template>
