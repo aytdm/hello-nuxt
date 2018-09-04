b6011d6317e323f7e14a9c890a166535b961b710
numa <aytdm251@gmail.com>
2018-09-04 18:35:07 +0900

    Inject component of SearchFom

--- pages/search.vue
+++ pages/search.vue
@@ -1,16 +1,7 @@
 <template>
   <div id="page_top">
     <el-container>
-      <el-main>
-        <el-form :inline="true" :model="searchForm" ref="searchForm" :rules="rules" @submit.native.prevent>
-          <el-form-item prop="keyword">
-            <el-input placeholder="search by keyword" prefix-icon="el-icon-search" v-model="searchForm.keyword"  @keyup.enter.native="search('searchForm')" />
-          </el-form-item>
-          <el-form-item>
-            <el-button @click="search('searchForm')">search</el-button>
-          </el-form-item>
-        </el-form>
-        <my-list :lists="lists" :hasData="hasData" />
+      <el-main v-loading.fullscreen.lock="isLoading">
       </el-main>
     </el-container>
   </div>
@@ -19,47 +10,19 @@
 <script lang="babel">
 import {mapState} from 'vuex'
 import MyList from '~/components/List.vue'
+import SearchForm from '~/components/SearchForm.vue'
 
 export default {
   layout: 'navbar',
   components: {
-    MyList
+    MyList,
+    SearchForm
   },
+  computed: mapState(['isLoading']),
   async fetch ({ store }) {
     store.dispatch('getItems', {
       keyword: 'nuxt.js'
     })
-  },
-  data () {
-    return {
-      searchForm: {
-        keyword: ''
-      },
-      rules: {
-        keyword: [
-          { required: true, message: 'Please input the keyword', trigger: 'blur' }
-        ]
-      }
-    }
-  },
-  computed: mapState([
-    'lists',
-    'hasData'
-  ]),
-  methods: {
-    search (form) {
-      this.$refs[form].validate((valid) => {
-        if (!valid) {
-          return false
-        }
-        this.sendRequest()
-      })
-    },
-    async sendRequest () {
-      this.$store.dispatch('getItems', {
-        keyword: this.searchForm.keyword
-      })
-    }
   }
 }
 </script>
