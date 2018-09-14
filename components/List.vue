4f21b2a83a8a681790cf077e6d25e560d7f11409
numa <aytdm251@gmail.com>
2018-09-14 15:08:59 +0900

    Use 'template' instead of 'div'.
    and remove unuse style.

--- components/List.vue
+++ components/List.vue
@@ -1,43 +1,43 @@
 <template>
-<div>
-  <div v-if="lists.length === 0 && !hasData">
-    <i class="el-icon-warning">&nbsp;No results found for your keyword.</i>
+  <div>
+    <template v-if="lists.length === 0 && !hasData">
+      <i class="el-icon-warning">&nbsp;No results found for your keyword.</i>
+    </template>
+    <template v-else>
+      <el-col :span="6" v-for="(element, index) in lists" :key="index" class="col-style">
+        <el-card :body-style="{ padding: '15px' }" class="box-card">
+          <div slot="header">
+            <a :href="element.url" target="_blank">{{ element.title }}</a>
+          </div>
+          <div class="bottom content-style text">
+            <div>{{ element.created_at }}</div>
+            <span>
+              <img :src="element.user.profile_image_url" width="15" height="15" />
+              <template v-if="element.user.description">
+                <el-popover slot="description" placement="top-start" width="300" trigger="hover" :content="element.user.description">
+                  <span slot="reference">&nbsp;{{ element.user.id }}</span>
+                </el-popover>
+              </template>
+              <template v-else>
+                <span>&nbsp;{{ element.user.id }}</span>
+              </template>
+            </span>
+            &nbsp;
+            <span>
+              <i class="el-icon-star-off">{{ element.likes_count }}</i>
+            </span>
+            <div>{{ getDescription(element.body) }}</div>
+            <el-tag size="mini" type="info" class="tab-style" v-for="(tag, index) in element.tags" :key="index">{{ tag.name }}</el-tag>
+          </div>
+        </el-card>
+      </el-col>
+      <template v-if="250 < scrollY" class="page-component-up">
+        <transition name="fade">
+          <i class="el-icon-caret-top" @click="scrollTop" />
+        </transition>
+      </template>
+    </template>
   </div>
-  <div v-else>
-    <el-col :span="6" v-for="(element, index) in lists" :key="index" class="col-style">
-      <el-card :body-style="{ padding: '15px' }" class="box-card">
-        <div slot="header" class="clearfix">
-          <a :href="element.url" target="_blank">{{ element.title }}</a>
-        </div>
-        <div class="bottom clearfix content-style text">
-          <div>{{ element.created_at }}</div>
-          <span>
-            <img :src="element.user.profile_image_url" width="15" height="15" />
-            <template v-if="element.user.description">
-              <el-popover slot="description" placement="top-start" width="300" trigger="hover" :content="element.user.description">
-                <span slot="reference">&nbsp;{{ element.user.id }}</span>
-              </el-popover>
-            </template>
-            <template v-else>
-              <span>&nbsp;{{ element.user.id }}</span>
-            </template>
-          </span>
-          &nbsp;
-          <span>
-            <i class="el-icon-star-off">{{ element.likes_count }}</i>
-          </span>
-          <div>{{ getDescription(element.body) }}</div>
-          <el-tag size="mini" type="info" class="tab-style" v-for="(tag, index) in element.tags" :key="index">{{ tag.name }}</el-tag>
-        </div>
-      </el-card>
-    </el-col>
-    <div v-if="250 < scrollY" class="page-component-up">
-      <transition name="fade">
-        <i class="el-icon-caret-top" @click="scrollTop" />
-      </transition>
-    </div>
-  </div>
-</div>
 </template>
 
 <script lang="babel">
@@ -67,16 +67,6 @@ export default {
 </script>
 
 <style>
-.clearfix:before,
-.clearfix:after {
-  display: table;
-  content: "";
-}
-
-.clearfix:after {
-  clear: both
-}
-
 .content-style {
   line-height: 30px;
 }
