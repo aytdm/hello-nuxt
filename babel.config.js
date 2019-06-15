45d0e97d4f492f995a70391850842f18aec2e657
numa <aytdm251@gmail.com>
2019-06-16 08:00:49 +0900

    chore: Update babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function (api) {
+  api && api.cache(false)
+
+  return {
+    presets: [
+      [
+        '@babel/env', {
+          'targets': {
+            'node': 'current'
+          }
+        }
+      ]
+    ]
+  }
+}
